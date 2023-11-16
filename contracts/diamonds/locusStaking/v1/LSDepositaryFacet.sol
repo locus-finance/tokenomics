27aadcb218e30df65790fd6a8ed408c88d40ad17
GoldenSylph <simplavero@gmail.com>
2023-11-16 18:13:39 +0400

    fix bug: only autocracts could stake

--- contracts/diamonds/locusStaking/v1/LSDepositaryFacet.sol
+++ contracts/diamonds/locusStaking/v1/LSDepositaryFacet.sol
@@ -12,6 +12,7 @@ import "../LSLib.sol";
 import "../../facetsFramework/tokensDistributor/TDLib.sol";
 import "../../facetsFramework/diamondBase/facets/BaseFacet.sol";
 import "../../locusToken/v1/interfaces/ILTERC20Facet.sol";
+import "../../locusToken/v1/autocracy/interfaces/ILTAutocracyFacet.sol";
 
 import "./interfaces/ILSProcessFeesFacet.sol";
 import "./interfaces/ILSDepositaryFacet.sol";
@@ -109,7 +110,7 @@ contract LSDepositaryFacet is
         LSLib.Primitives storage p = LSLib.get().p;
         IERC20 stakingToken = p.stakingToken;
         address locusToken = p.locusToken;
-        if (address(stakingToken) == locusToken) {
+        if (address(stakingToken) == locusToken && !ILTAutocracyFacet(locusToken).areAutocratsReign()) {
             // WARNING: CHECK THE ROLE OF STAKING DIAMOND: VOTING_POWER_DISTRIBUTOR
             ILTERC20Facet(locusToken).lowLevelDelegate(staker, staker);
         }
