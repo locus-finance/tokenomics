6998b8c6942716bb501824fda070e2c910a3d445
GoldenSylph <simplavero@gmail.com>
2023-11-29 22:01:24 +0400

    mint for midas claim

--- tasks/autocracyMintLocus.js
+++ tasks/autocracyMintLocus.js
@@ -4,7 +4,7 @@ module.exports = (task) =>
     "mint",
     "Mint LOCUS being an autocrat to the fundraising address.",
   )
-    .addOptionalParam("amount", "Define amount to be minted.", '529959380000000000000', types.string)
+    .addOptionalParam("amount", "Define amount to be minted.", '3300000000000000000000000', types.string)
     .addOptionalParam("address", "Define address to be minted to.", '0x445816ac3E78D1B0547b4642b373A88aD875cc8a', types.string)
     .setAction(async ({ amount, address }, hre) => {
       const signers = await hre.ethers.getSigners();
