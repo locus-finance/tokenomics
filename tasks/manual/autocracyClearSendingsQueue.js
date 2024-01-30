6071d19807c299ae99884c8619f305b305e165bc
Oleg Bedrin <numeralhuman@gmail.com>
2024-01-30 16:03:05 +0500

    fix clearing queue

--- tasks/manual/autocracyClearSendingsQueue.js
+++ tasks/manual/autocracyClearSendingsQueue.js
@@ -10,8 +10,9 @@ module.exports = (task) =>
         "DiamondLocusStaking",// hre.names.internal.diamonds.locusStaking.interface,
         (await hre.deployments.get("LocusStaking_DiamondProxy")).address
       );
+      let processQueueTx;
       if ((await locusStakingInstance.getDequeSize()).gt(0)) {
-        const processQueueTx = await locusStakingInstance.connect(deployer).processQueue();
+        processQueueTx = await locusStakingInstance.connect(deployer).processQueue();
         await processQueueTx.wait();
       }
       console.log(`The queue has been cleared:\n${JSON.stringify(processQueueTx)}`);
