8e49cb462677c1fa364573b8f4c5c4027aed3242
Oleg Bedrin <numeralhuman@gmail.com>
2024-01-31 14:02:43 +0500

    adding test template

---
+++ test/diamonds/locusStaking/LSDepositaryFacet.test.js
@@ -0,0 +1,56 @@
+const { expect } = require("chai");
+const networkHelpers = require("@nomicfoundation/hardhat-network-helpers");
+const hre = require("hardhat");
+const keccak256 = require('keccak256');
+const { deployments, getNamedAccounts } = hre;
+const { WEEK, withImpersonatedSigner, mintNativeTokens } = require("../deploy/helpers");
+
+describe("DiamondLocusStaking", () => {
+  let namedAccounts;
+  let person;
+  let personBalance;
+  let totalReward;
+
+  let locusStaking;
+  let locusToken;
+
+  beforeEach(async () => {
+    namedAccounts = await getNamedAccounts();
+    await deployments.fixture([
+      "debug"
+    ]);
+
+    locusStaking = await hre.ethers.getContractAt(
+      hre.names.internal.diamonds.locusStaking.interface,
+      (await deployments.get(hre.names.internal.diamonds.locusStaking.proxy)).address
+    );
+    locusToken = await hre.ethers.getContractAt(
+      hre.names.internal.diamonds.locusToken.interface,
+      (await deployments.get(hre.names.internal.diamonds.locusToken.proxy)).address
+    );
+
+    // some random person
+    person = "0x35AC85c14Be0acc68870348F33B150364aF35303";
+
+    // a real total supply from onchain arbitrumOne
+    personBalance = hre.ethers.BigNumber.from('1954286826623793992312644');
+    
+    // a real total reward acquired from onchain arbitrumOne
+    totalReward = hre.ethers.BigNumber.from('32499999999999998323200');
+    
+    await locusToken.mint(person, personBalance);
+    await locusToken.mint(namedAccounts.deployer, totalReward);
+
+    await locusToken.approve(locusStaking.address, totalReward);
+    await locusStaking.notifyRewardAmount(totalReward);
+
+    await withImpersonatedSigner(person, async (personSigner) => {
+      await mintNativeTokens(personSigner, "0x1000000000000000000000");
+      await locusToken.connect(personSigner).approve(locusStaking.address, personBalance);
+      await locusStaking.connect(personSigner).stake(personBalance);
+    });
+
+    await locusStaking.grantRole(namedAccounts.deployer, keccak256('DELAYED_SENDINGS_QUEUE_PROCESSOR_ROLE'));
+  });
+
+});
