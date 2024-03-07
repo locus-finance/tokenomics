bf7a29e3030ed2a1505e4c25e9a4c094ed401b1b
Oleg Bedrin <numeralhuman@gmail.com>
2024-03-07 15:04:35 +0400

    fix task duplicate

--- tasks/manual/stakingOperations/stake.js
+++ tasks/manual/stakingOperations/stake.js
@@ -2,17 +2,31 @@ const { types } = require("hardhat/config");
 module.exports = (task) =>
   task(
     "stake",
-    "Call stake(...) on staking contract.",
+    "Stake for the deployer.",
   )
-    .addOptionalParam("amount", "Define an amount to be staked.", '', types.string)
+    .addOptionalParam("token", "Define a name of staking token in hre.names.", '', types.string)
     .addOptionalParam("staking", "Define a name of staking contract in hre.names.", '', types.string)
-    .setAction(async ({ amount, staking }, hre) => {
+    .addOptionalParam("amount", "Define amount to be staked.", '32500000000000000000000', types.string)
+    .setAction(async ({ amount, token, staking }, hre) => {
       await hre.names.gather();
+      
       staking = staking !== '' ? staking : hre.names.internal.diamonds.locusStaking.proxy;
-      const stakingInstance = await hre.ethers.getContractAt(
+      token = token !== '' ? token : hre.names.internal.diamonds.locusToken.proxy;
+
+      const locusStakingInstance = await hre.ethers.getContractAt(
         hre.names.internal.diamonds.locusStaking.interface,
         (await hre.deployments.get(staking)).address
       );
-      const result = await stakingInstance.stake(amount);
-      console.log(`Stake called:\n${JSON.stringify(result)}`);
+      const locusToken = await hre.ethers.getContractAt(
+        hre.names.internal.diamonds.locusToken.interface,
+        (await hre.deployments.get(token)).address
+      );
+
+      const approveTx = await locusToken.approve(locusStakingInstance.address, amount);
+      await approveTx.wait();
+      const stakeTx = await locusStakingInstance.stake(amount);
+      await stakeTx.wait();
+
+      console.log(`Tx:\n${JSON.stringify(stakeTx)}`);
+      console.log(`Staked LOCUS' amount ${hre.ethers.utils.formatUnits(amount)}`);
     });
