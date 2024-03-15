6d2aefb07b6879093fbd536af2c113a51c91db4f
Oleg Bedrin <numeralhuman@gmail.com>
2024-03-15 20:36:50 +0400

    fix provide

--- tasks/manual/autocracy/provideRewardForStaking.js
+++ tasks/manual/autocracy/provideRewardForStaking.js
@@ -5,13 +5,14 @@ module.exports = (task) =>
     "provide",
     "Provide LOCUS' for the staking contract.",
   )
-    // amount for autoreflective token is 1083333333333333333334 and has to be called once a day
-    // amount for autoreflective token is 1778563941240000000000 and it has to be until 15 of april 2024
-    // amount for autoreflective token is 3250000000000000000000
+    // wei amount for autoreflective token is 1083333333333333333334 and has to be called once a day
+    // wei amount for autoreflective token is 1778563941240000000000 and it has to be until 15 of april 2024
+    // wei amount for autoreflective token is 3250000000000000000000
     .addOptionalParam("amount", "Define amount to be provided.", '0', types.string)
     .addOptionalParam("staking", "Define a custom name of Diamond Staking from hre.names.", '', types.string)
     .addOptionalParam("locus", "Define a name of Diamond Locus Token from hre.names.", '', types.string)
-    .setAction(async ({ amount, staking, locus }, hre) => {
+    .addOptionalParam("confirmations", "An amount of confirmations to wait.", 10, types.int)
+    .setAction(async ({ amount, staking, locus, confirmations }, hre) => {
       if (amount === "0") throw RuntimeError("Nothing to provide. Zero is past as amount parameter.");
       const signers = await hre.ethers.getSigners();
       const deployer = signers[0].address;
@@ -36,9 +37,10 @@ module.exports = (task) =>
         (await hre.deployments.get(locusTokenName)).address
       );
 
-      const approveTx = await locusToken.approve(autoreflectiveStaking.address, amount);
-      await approveTx.wait();
-      const notifyRewardAmountTx = await autoreflectiveStaking.notifyRewardAmount(amount);
-      await notifyRewardAmountTx.wait();
+      const amountWei = hre.ethers.utils.parseEther(amount);
+      const approveTx = await locusToken.approve(autoreflectiveStaking.address, amountWei);
+      await approveTx.wait(confirmations);
+      const notifyRewardAmountTx = await autoreflectiveStaking.notifyRewardAmount(amountWei);
+      await notifyRewardAmountTx.wait(confirmations);
       console.log(`Success: notifyRewardAmount(${amount}) called:\n${JSON.stringify(notifyRewardAmountTx)}`);
     });
