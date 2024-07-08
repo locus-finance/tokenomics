fdd1a84c121a3676ca42b476757b374f20962c91
Oleg Bedrin <numeralhuman@gmail.com>
2024-07-09 00:15:38 +0400

    adding more tempo tests

--- test/AnyFixture.test.js
+++ test/AnyFixture.test.js
@@ -9,15 +9,6 @@ const { WEEK, withImpersonatedSigner, mintNativeTokens } = require("../deploy/he
 // ALLOWED TO SMELL AND BE LITTERED
 describe("AnyFixture", () => {
 
-  // it('should', async () => {
-  //   await hre.run('provide', {
-  //     staking: "0xFCE625E69Bd4952417Fe628bC63D9AA0e4012684",
-  //     locus: "0xe1d3495717f9534Db67A6A8d4940Dd17435b6A9E",
-  //     amount: "1083.333333333333333334",
-  //     confirmations: 0
-  //   });
-  // });
-
   // it('should collect balances', async () => {
   //   const addresses = [
   //     "0x722982c9A02e2571D245f18E460914E884AdC892",
@@ -73,13 +64,58 @@ describe("AnyFixture", () => {
   //   console.log(stLocusBalances);
   // });
 
-  // it('should test', async () => {
+  it('should provide', async () => {
+    await hre.names.gather();
+    // const staking = await hre.ethers.getContractAt(
+    //   hre.names.internal.diamonds.locusStaking.interface,
+    //   "0x24d6D6af23Cd865B4Dee7f169CA60Bf07B4DD9AE"
+    // );
+  })
+
+  // it('should clear the deque of sendings', async () => {
   //   await hre.names.gather();
   //   const staking = await hre.ethers.getContractAt(
-  //     hre.names.internal.diamonds.locusXUsdTokensStaking.interface,
-  //     "0x6C447230F098CDdB62f6AEaeEc25C27E8b90B25e"
+  //     hre.names.internal.diamonds.locusStaking.interface,
+  //     "0x24d6D6af23Cd865B4Dee7f169CA60Bf07B4DD9AE"
   //   );
-  //   console.log(await staking.getPrimitives());
+  //   await staking.processQueue();
+  // });
+  
+  // it('should test', async () => {
+  //   const stakingInfos = [
+  //     {
+  //       address: "0x91A894C32B14F26f708389E5F8e21964b7d3C025",
+  //       name: "xDefiStaking"
+  //     },
+  //     {
+  //       address: "0x6C447230F098CDdB62f6AEaeEc25C27E8b90B25e",
+  //       name: "xArbStaking"
+  //     },
+  //     {
+  //       address: "0xCC50DC869546524E675121fC331249727A549027",
+  //       name: "xEthStaking"
+  //     },
+  //     {
+  //       address: "0x6390743ccb7928581F61427652330a1aEfD885c2",
+  //       name: "xUsdStaking" 
+  //     },
+  //     {
+  //       address: "0x24d6D6af23Cd865B4Dee7f169CA60Bf07B4DD9AE",
+  //       name: "pendleEthStaking"
+  //     }
+  //   ];
+  //   await hre.names.gather();
+  //   for (const stakingInfo of stakingInfos) {
+  //     console.log(`--- Gathering info for ${stakingInfo.name} --`)
+  //     const staking = await hre.ethers.getContractAt(
+  //       hre.names.internal.diamonds.locusStaking.interface,
+  //       stakingInfo.address
+  //     );
+  //     const deque = await staking.getSendingsDeque();
+  //     console.log('Sendings queue:');
+  //     console.log(deque.length);
+  //     console.log(`--- Gathered info for ${stakingInfo.name} --`)
+  //   }
   // });
 
   // it('should change balances', async () => {
